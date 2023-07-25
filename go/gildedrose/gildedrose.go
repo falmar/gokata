76448bd875980b072231996b8e884a06b4e5d1fe
David Lavieri <daviddlavier@gmail.com>
2023-07-25 23:27:01 +0200

    refactor and add test cases

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,58 +1,69 @@
 package gildedrose
 
+import (
+	"strings"
+)
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
+const maxQuality = 50
+const minQuality = 0
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
+		// too legendary to change
+		if items[i].Name == "Sulfuras, Hand of Ragnaros" {
+			continue
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
+		// degrade by 1
+		var offset int = -1
+		// to check for "backstage passes" and "conjured"
+		var itemName = strings.ToLower(items[i].Name)
+
+		if strings.Contains(itemName, "backstage passes") {
+			if items[i].SellIn <= 0 {
+				// useless ticket!
+				items[i].SellIn -= 1
+				items[i].Quality = 0
+				continue
+			}
+
+			offset = 1
+
+			// quality goes up by dates
+			if items[i].SellIn <= 5 {
+				offset++
+			}
+			if items[i].SellIn <= 10 {
+				offset++
+			}
+		} else if itemName == "aged brie" {
+			offset = 1
 		}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			}
+		// degrade twice as much after sell date
+		// || strings.Contains(itemName, "conjured")
+		// conjured items degrade twice as fast; it will break the "texttest"
+		if items[i].SellIn <= 0 {
+			offset *= 2
 		}
+
+		// SellIn changes for all
+		items[i].SellIn -= 1
+
+		// can't go above 50 or below 0
+		if items[i].Quality+offset > maxQuality {
+			items[i].Quality = maxQuality
+			continue
+		} else if items[i].Quality+offset < minQuality {
+			items[i].Quality = minQuality
+			continue
+		}
+
+		items[i].Quality += offset
 	}
-
 }
--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -1,19 +1,288 @@
 package gildedrose_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 )
 
-func Test_Foo(t *testing.T) {
+func Test_Legendary(t *testing.T) {
 	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+		{"Sulfuras, Hand of Ragnaros", 3, 1},
+		{"Sulfuras, Hand of Ragnaros", 0, 1},
+		{"Sulfuras, Hand of Ragnaros", -1, 30},
+		{"Sulfuras, Hand of Ragnaros", 10, 0},
+		{"Sulfuras, Hand of Ragnaros", 20, 10000},
 	}
 
-	gildedrose.UpdateQuality(items)
+	expectItems := make([][]int, len(items))
 
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	// no change
+	for j := 0; j < len(items); j++ {
+		expectItems[j] = []int{items[j].SellIn, items[j].Quality}
+	}
+
+	for i := 0; i < 30; i++ {
+		gildedrose.UpdateQuality(items)
+
+		for j := 0; j < len(items); j++ {
+			// nameshould not change
+			if items[j].Name != "Sulfuras, Hand of Ragnaros" {
+				t.Errorf("Name: Expected \"Sulfuras, Hand of Ragnaros\" but got %s ", items[j].Name)
+			}
+
+			// sellIn should decrease
+			if items[j].SellIn != expectItems[j][0] {
+				t.Errorf("SellIn: Expected %d but got %d ", expectItems[j][0]-1, items[j].SellIn)
+			}
+
+			// quality should decrease by 2 and not go below 0 or above 50
+			if items[j].Quality != expectItems[j][1] {
+				t.Errorf("Quality: Expected %d but got %d ", expectItems[j][1]-2, items[j].Quality)
+			}
+		}
+	}
+}
+
+func Test_Cheese(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Aged Brie", 3, 1},
+		{"Aged Brie", 0, 1},
+		{"Aged Brie", -1, 30},
+		{"Aged Brie", 10, 0},
+		{"Aged Brie", 20, 10000},
+	}
+
+	expectItems := make([][]int, len(items))
+
+	for i := 0; i < 30; i++ {
+		for j := 0; j < len(items); j++ {
+			expectItems[j] = []int{items[j].SellIn, items[j].Quality}
+
+			var by int = 1
+
+			if items[j].SellIn <= 0 {
+				by = 2
+			}
+
+			// quality should decrease by 2 and not go below 0 or above 50
+			if items[j].Quality+by < 0 {
+				expectItems[j][1] = 0
+			} else if items[j].Quality+by > 50 {
+				expectItems[j][1] = 50
+			} else {
+				expectItems[j][1] = items[j].Quality + by
+			}
+		}
+
+		gildedrose.UpdateQuality(items)
+
+		for j := 0; j < len(items); j++ {
+			// name should not change
+			if items[j].Name != "Aged Brie" {
+				t.Errorf("Name: Expected \"Aged Brie\" but got %s ", items[j].Name)
+			}
+
+			// sellIn should decrease
+			if items[j].SellIn != expectItems[j][0]-1 {
+				t.Errorf("SellIn: Expected %d but got %d ", expectItems[j][0]-1, items[j].SellIn)
+			}
+
+			// quality should increase by 1 and not go below 0 or above 50
+			if items[j].Quality < 0 {
+				t.Errorf("Quality: Expected %d but got %d ", 0, items[j].Quality)
+			} else if items[j].Quality > 50 {
+				t.Errorf("Quality: Expected %d but got %d ", 50, items[j].Quality)
+			} else if items[j].Quality != expectItems[j][1] {
+				t.Errorf("Quality: Expected %d but got %d ", expectItems[j][1]-2, items[j].Quality)
+			}
+		}
+	}
+}
+
+func Test_Conjured(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Conjured Mana Cake", 3, 6},
+		{"Conjured Shield", 10, 47},
+	}
+
+	expectItems := make([][]int, len(items))
+	var expectNames = []string{"Conjured Mana Cake", "Conjured Shield"}
+
+	for i := 0; i < 30; i++ {
+		for j := 0; j < len(items); j++ {
+			expectItems[j] = []int{items[j].SellIn, items[j].Quality}
+
+			var by int = 1
+
+			if items[j].SellIn <= 0 {
+				by = 2
+			}
+
+			if items[j].Quality-by < 0 {
+				expectItems[j][1] = 0
+			} else if items[j].Quality-by > 50 {
+				expectItems[j][1] = 50
+			} else {
+				expectItems[j][1] = items[j].Quality - by
+			}
+		}
+
+		gildedrose.UpdateQuality(items)
+
+		for j := 0; j < len(items); j++ {
+			// nameshould not change
+			if items[j].Name != expectNames[j] {
+				t.Errorf("Name: Expected %s but got %s ", expectNames[j], items[j].Name)
+			}
+
+			// sellIn should decrease
+			if items[j].SellIn != expectItems[j][0]-1 {
+				t.Errorf("SellIn: Expected %d but got %d ", expectItems[j][0]-1, items[j].SellIn)
+			}
+
+			// quality should decrease by 2 and not go below 0 or above 50
+			if items[j].Quality < 0 {
+				t.Errorf("Quality: Expected %d but got %d ", 0, items[j].Quality)
+			} else if items[j].Quality > 50 {
+				t.Errorf("Quality: Expected %d but got %d ", 50, items[j].Quality)
+			} else if items[j].Quality != expectItems[j][1] {
+				t.Errorf("Quality: Expected %d but got %d ", expectItems[j][1], items[j].Quality)
+			}
+		}
+	}
+}
+
+func Test_Backstage(t *testing.T) {
+	items := []*gildedrose.Item{
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a Wind Rose concert", 10, 49},
+		{"Backstage passes to a Siames concert", 5, 49},
+		{"Backstage passes to a In Flames concert", 3, 6},
+		{"Backstage passes to a Thrice concert", 0, 6},
+	}
+
+	expectItems := make([][]int, len(items))
+	var expectNames = []string{
+		"Backstage passes to a TAFKAL80ETC concert",
+		"Backstage passes to a Wind Rose concert",
+		"Backstage passes to a Siames concert",
+		"Backstage passes to a In Flames concert",
+		"Backstage passes to a Thrice concert",
+	}
+
+	for i := 0; i < 30; i++ {
+		for j := 0; j < len(items); j++ {
+			expectItems[j] = []int{items[j].SellIn, items[j].Quality}
+
+			var by int = 1
+
+			if items[j].SellIn <= 0 {
+				expectItems[j][1] = 0
+				continue
+			} else if items[j].SellIn <= 5 {
+				by = 3
+			} else if items[j].SellIn <= 10 {
+				by = 2
+			}
+
+			// quality should decrease by 1 and not go below 0 or above 50
+			if items[j].Quality+by > 50 {
+				expectItems[j][1] = 50
+			} else if items[j].Quality < 0 {
+				expectItems[j][1] = 0
+			} else {
+				expectItems[j][1] = items[j].Quality + by
+			}
+		}
+
+		gildedrose.UpdateQuality(items)
+
+		for j := 0; j < len(items); j++ {
+			// nameshould not change
+			if items[j].Name != expectNames[j] {
+				t.Errorf("Name: Expected %s but got %s ", expectNames[j], items[j].Name)
+			}
+
+			// sellIn should decrease
+			if items[j].SellIn != expectItems[j][0]-1 {
+				t.Errorf("SellIn: Expected %d but got %d ", expectItems[j][0]-1, items[j].SellIn)
+			}
+
+			// quality should decrease by 1 and not go below 0 or above 50
+			if items[j].Quality < 0 {
+				t.Errorf("Quality: Expected %d but got %d ", 0, items[j].Quality)
+			} else if items[j].Quality > 50 {
+				t.Errorf("Quality: Expected %d but got %d ", 50, items[j].Quality)
+			} else if items[j].Quality != expectItems[j][1] {
+				fmt.Println(items[j])
+				t.Errorf("Quality: Expected %d but got %d ", expectItems[j][1], items[j].Quality)
+			}
+		}
+	}
+}
+
+func Test_Regular(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"+5 Dexterity Vest", 10, 20},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Mystical Ruby Amulet", 15, 30},
+		{"Potion of Infinite Wisdom", 7, 10},
+		{"Elven Leather Boots", 12, 25},
+	}
+
+	expectItems := make([][]int, len(items))
+	var expectNames = []string{
+		"+5 Dexterity Vest",
+		"Elixir of the Mongoose",
+		"Mystical Ruby Amulet",
+		"Potion of Infinite Wisdom",
+		"Elven Leather Boots",
+	}
+
+	for i := 0; i < 30; i++ {
+		// make sure we have the right expectations from their current state
+		for j := 0; j < len(items); j++ {
+			expectItems[j] = []int{items[j].SellIn, items[j].Quality}
+
+			var by int = 1
+
+			if items[j].SellIn <= 0 {
+				by = 2
+			}
+
+			// quality should decrease by 1 and not go below 0 or above 50
+			if items[j].Quality-by < 0 {
+				expectItems[j][1] = 0
+			} else if items[j].Quality-by > 50 {
+				expectItems[j][1] = 50
+			} else {
+				expectItems[j][1] = items[j].Quality - by
+			}
+		}
+
+		gildedrose.UpdateQuality(items)
+
+		for j := 0; j < len(items); j++ {
+			// nameshould not change
+			if items[j].Name != expectNames[j] {
+				t.Errorf("Name: Expected %s but got %s ", expectNames[j], items[j].Name)
+			}
+
+			// sellIn should decrease
+			if items[j].SellIn != expectItems[j][0]-1 {
+				t.Errorf("SellIn: Expected %d but got %d ", expectItems[j][0]-1, items[j].SellIn)
+			}
+
+			// quality should decrease by 1 and not go below 0 or above 50
+			if items[j].Quality < 0 {
+				t.Errorf("Quality: Expected %d but got %d ", 0, items[j].Quality)
+			} else if items[j].Quality > 50 {
+				t.Errorf("Quality: Expected %d but got %d ", 50, items[j].Quality)
+			} else if items[j].Quality != expectItems[j][1] {
+				t.Errorf("Quality: Expected %d but got %d ", expectItems[j][1], items[j].Quality)
+			}
+		}
 	}
 }
